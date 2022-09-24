05230a56ccd594b020ee9558ed39b1020d133a71
Mirza Muhammad Baig <104078606+MirzaMuhammadBaig@users.noreply.github.com>
2022-09-24 17:54:44 +0500

    Initial commit

--- scripts/getAdminMinting.js
+++ scripts/getAdminMinting.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const Web3 = require('web3');
 const MyContract = require('../build/contracts/specialNftContract.json');
-const { API_URL_INFURA , CONTRCT_ADDRESS } = process.env;
+const { AlchemyAPI , CONTRCT_ADDRESS } = process.env;
 
 async function AdminMinting() {
-    const web3 = new Web3(API_URL_INFURA);
+    const web3 = new Web3(AlchemyAPI);
     
     
     let contract = new web3.eth.Contract(
