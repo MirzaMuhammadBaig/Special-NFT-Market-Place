001e9fb8768fa90c15e3147dddd7434ce39809c6
Mirza Muhammad Baig <104078606+MirzaMuhammadBaig@users.noreply.github.com>
2022-09-24 17:53:13 +0500

    Initial commit

--- scripts/getAdminAddress.js
+++ scripts/getAdminAddress.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const Web3 = require('web3');
 const MyContract = require('../build/contracts/specialNftContract.json');
-const { API_URL_INFURA , CONTRCT_ADDRESS } = process.env;
+const { AlchemyAPI , CONTRCT_ADDRESS } = process.env;
 
 async function CheckAdminAddress() {
-    const web3 = new Web3(API_URL_INFURA);
+    const web3 = new Web3(AlchemyAPI);
     
     
     let contract = new web3.eth.Contract(
@@ -14,7 +14,7 @@ async function CheckAdminAddress() {
         );
 
     const AdminMinting = await contract.methods
-    .CheckAdminAddress('0xc9cf24A1145233d8607a6c996373c05dBC2397D4')
+    .CheckAdminAddress(yourAdminAddress)
     .call();
     console.log(`Tx is succesfull.`);
     console.log(`Your value is ${AdminMinting}`)
