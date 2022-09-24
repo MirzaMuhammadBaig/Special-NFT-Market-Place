229e2aba8abfcb6e5686dbec54a1444bf0f82774
Mirza Muhammad Baig <104078606+MirzaMuhammadBaig@users.noreply.github.com>
2022-09-24 17:59:38 +0500

    Initial commit

--- scripts/setAdminMinting.js
+++ scripts/setAdminMinting.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const Web3 = require('web3');
-const { API_URL_INFURA , CONTRCT_ADDRESS , PRIVATE_KEY , PUBLIC_KEY} = process.env;
+const { AlchemyAPI , CONTRCT_ADDRESS , PRIVATE_KEY , PUBLIC_KEY} = process.env;
 const MyContract = require('../build/contracts/specialNftContract.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 async function ActiveAdminMinting() {
-    const provider = new HDWalletProvider(PRIVATE_KEY, API_URL_INFURA);
+    const provider = new HDWalletProvider(PRIVATE_KEY, AlchemyAPI);
 
     const web3 = new Web3(provider);
     
